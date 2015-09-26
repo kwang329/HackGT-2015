34d2df0719c472b177ad9e0bb7297b28940c5122
Ojan Thornycroft <othornycroft@gmail.com>
2015-09-26 15:04:10 -0400

    Modified location updater to display updated locations

--- app/src/main/java/hackgt/directionapp/LocUpdater.java
+++ app/src/main/java/hackgt/directionapp/LocUpdater.java
@@ -9,15 +9,42 @@ import android.app.Activity;
 import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 /**
  * Contains the methods for continuous location updates
  * Created by Kaiwen on 9/26/2015.
  */
-public class LocUpdater extends Activity {
+public class LocUpdater extends Activity implements GoogleApiClient.ConnectionCallbacks, LocationListener,
+        GoogleApiClient.OnConnectionFailedListener {
+
     private GoogleApiClient mGoogleApiClient;
     private double lat = 31.5; //Our default location is now the Dead Sea. Because I feel like it.
     private double lng = 35.5;
+    private LocationRequest mLocationRequest = new LocationRequest();
+    private int counter = 0;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        buildGoogleApiClient();
+        createLocationRequest();
+        setContentView(R.layout.activity_loc_updater);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    public void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API).build();
+    }
 
     public void onConnected(Bundle connectionHint) {
         Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
@@ -25,9 +52,16 @@ public class LocUpdater extends Activity {
             lat = mLastLocation.getLatitude();
             lng = mLastLocation.getLongitude();
         }
+        TextView locationLabel = (TextView) findViewById(R.id.locationLabel);
+        locationLabel.setText("{" + lat + ", " + lng + ")");
+        startLocationUpdates();
     }
 
-    LocationRequest mLocationRequest = new LocationRequest();
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
 
     //Should be accurate enough for real time data
     protected void createLocationRequest() {
@@ -38,6 +72,21 @@ public class LocUpdater extends Activity {
     }
 
     protected void startLocationUpdates() {
-        //LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        TextView locationLabel = (TextView) findViewById(R.id.locationLabel);
+        locationLabel.setText(locationLabel.getText() + "\n(" + location.getLatitude()
+                + ", " + location.getLongitude() + ")");
+        TextView counterLabel = (TextView) findViewById(R.id.counter);
+        counter++;
+        counterLabel.setText("" + counter);
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
     }
 }
